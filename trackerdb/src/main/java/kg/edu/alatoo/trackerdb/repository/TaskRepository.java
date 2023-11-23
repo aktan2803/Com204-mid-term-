fe48b99391bc103171b97fa718ce288e6e36d925
aktan2803 <81546800+aktan2803@users.noreply.github.com>
2023-11-23 17:25:09 +0600

    Delete trackerdb directory

--- trackerdb/src/main/java/kg/edu/alatoo/trackerdb/repository/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package kg.edu.alatoo.trackerdb.repository;
-
-import kg.edu.alatoo.trackerdb.entity.Task;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TaskRepository extends CrudRepository<Task, Long> {
-}
--- trackerdb/src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO task (description, issuer, executor, status) VALUES ('Coffee', 'Mark', 'Bill', 0);
-INSERT INTO task (description, issuer, executor, status) VALUES ('Mobile app', 'Elon', 'Mark', 1);
